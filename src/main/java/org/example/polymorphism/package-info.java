bbb5ed9bf3aae5bd38abf89533cdfaa308d30d41
MrWanted <wanted.lepota@gmail.com>
2023-01-31 20:31:33 +0200

    polymorphism examples

--- src/main/java/org/example/polymorphism/package-info.java
+++ src/main/java/org/example/polymorphism/package-info.java
@@ -1,4 +1,6 @@
 package org.example.polymorphism;
 /****
- *
+ *Polymorphism: It is a feature of OOP that allows objects of different classes to be treated as objects of a common class.
+ * This enables you to write code that can handle objects of various types, using the same method calls,
+ * without needing to know the specific type of object it is dealing with.
  */
