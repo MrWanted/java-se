7a385c4b288de0d7727d7815b7e99a044dc86145
MrWanted <wanted.lepota@gmail.com>
2023-02-26 08:16:17 +0200

    move

--- src/main/java/org/example/designpatterns/singleton/package-info.java
+++ src/main/java/org/example/designpatterns/singleton/package-info.java
@@ -1,4 +1,4 @@
-package org.example.singleton;
+package org.example.designpatterns.singleton;
 /**
  * The Singleton design pattern is a software design pattern that restricts the instantiation of a class to only one object
  * and provides a global point of access to that object throughout the application.
---
+++ src/main/java/org/example/designpatterns/staticfactorymethod/Adult.java
@@ -0,0 +1,4 @@
+package org.example.designpatterns.staticfactorymethod;
+
+public class Adult {
+}
